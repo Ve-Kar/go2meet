45dce9bc5db297a46539ea4a13263d7172077b34
Veselin Karayanev <109144624+Ve-Kar@users.noreply.github.com>
2022-11-07 16:58:26 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter,  Route, Routes } from 'react-router-dom';
 import { FavoritesContextProvider } from './store/favorites-contex';
-// import Login from './pages/Login';
-// import Dashboard from './pages/Dashboard';
+
 
 
 
